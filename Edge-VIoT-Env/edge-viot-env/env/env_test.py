c8ec06d6c4fb45dd5737170cf4ea9be9b3caf74e
QQisQQ <1399858762@qq.com>
2025-01-29 01:26:49 +0800

    clear deprecated

--- Edge-VIoT-Env/edge-viot-env/env/env_test.py
+++
@@ -1,7 +0,0 @@
-from edge_viot_env import EdgeVIoTEnv, raw_env
-from pettingzoo.test import parallel_api_test, api_test
-
-if __name__ == "__main__":
-    env = EdgeVIoTEnv(render_mode="ansi")
-    parallel_api_test(env, num_cycles=1_000_000)
-    api_test(raw_env())
