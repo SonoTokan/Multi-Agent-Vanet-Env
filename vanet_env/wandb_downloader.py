b5f7cb2bf60cbc40dbb8d7aebaebd9b680d2ff09
SonoTokan <40739351+SonoTokan@users.noreply.github.com>
2025-04-01 14:42:56 +0800

    Update wandb_downloader.py
    delete wangdb private information

--- vanet_env/wandb_downloader.py
+++ vanet_env/wandb_downloader.py
@@ -6,8 +6,8 @@ run = wandb.init()
 
 api = wandb.Api()
 
-# 获取运行数据
-run_path = "1399858762-fjnu/Vanet/runs/y0smpfn0"
+# 获取运行数据, url
+run_path = "name/xxxx/runs/xxxxxxx"
 run = api.run(run_path)
 
 # 获取历史数据
